0b66787c7bd1ee648e9e964058ab4ae02f1e7984
Sushmita Anil Maurya <mauryasushmita9422@gmail.com>
2026-08-28 22:01:41 +0530

    fix production login signup

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,13 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [ENV.CLIENT_URL, "http://localhost:5173"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://quick-chatt-kappa.vercel.app"
+  ],
+  credentials: true
+}));
 
 /* ---------------- HEALTH CHECK ROUTE ---------------- */
 
